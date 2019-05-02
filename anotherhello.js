3991ebfffcef8313923b7dc81e73aee35f7a2065
Carlos R. Arias <arias@spu.edu>
2019-05-02 09:55:22 -0700

    with message

--- anotherhello.js
+++ anotherhello.js
@@ -2,16 +2,18 @@
 var express = require('express');
 // creates the express application
 var app = express();
-// handles a GET request with route
+// handles a GET request with route /
 app.get('/', function ServerListener(request, response) {
     // response object sends the text
     response.send('Hello From Express');
     const { headers, method, url } = request;
     // shows on the server console that a request was made
-    console.log('Request ( ' + headers + ' ' + url + ' ' + method + ') ' +
+    console.log('Request ( ' + JSON.stringify(headers) + ' ' + url + ' ' + method + ') ' +
         'from ' + request.connection.remoteAddress);
 });
 
-app.listen(3000, function() {
-    console.log('Express Listening on http://leia.cs.spu.edu:3000');
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Express Listening on http://%s:%s', host, port);
 });
--- helloworld.js
+++ helloworld.js
@@ -3,11 +3,12 @@ var app = express();
 
 app.get('/', function ServerListener(request, response) {
     response.send('Hello World');
-})
+    console.log("Got a request from %s", request.ip);
+});
 
 var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
 
     console.log("Example app listening at http://%s:%s", host, port)
-})
+});
